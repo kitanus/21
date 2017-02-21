dd724606f14707ad2b3e9afe0f8d428a5c5f1d9b
kitanus <kulukafarmin@mail.ru>
2017-02-21 17:35:22 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+var monk = [];
+function getRandomInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function Randrand(mond){
+for(i=0; i<11; i++){
+  num = getRandomInt(1, 11);
+  h=1;
+  while(h == 1){
+    k=0; j=0;
+    while(k < 11){
+      if(mond[k] === num){
+        j++;
+      }
+      k++;
+    }
+    if(j === 0){
+      mond[i]=num;
+      h=2;
+    } else {
+      num = getRandomInt(1, 11);
+    }
+  }
+}
+return mond;
+}
+
+var doc = document,
+    card_evil=doc.getElementsByClassName("card_evil"),
+    card_hero=doc.getElementsByClassName("card_hero"),
+    amount_evil=doc.getElementById("amount_evil"),
+    amount_hero=doc.getElementById("amount_hero"),
+    win_evil=doc.getElementById("win_evil"),
+    win_hero=doc.getElementById("win_hero"),
+    more=doc.getElementById("more"),
+    pass=doc.getElementById("pass");
+
+var rand_num = Randrand(monk);
+alert(rand_num);
