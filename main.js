037d61ad7bbd8aef55f83791fc1873862ffc6d15
kitanus <kulukafarmin@mail.ru>
2017-02-22 15:53:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
 var monk = [];
+
 // Функция для поиска случайных целых чисел
 function getRandomInt(min, max)
 {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 // Функция для поиска случайных целых чисел без повторений
 function Randrand(mond){
-for(i=0; i<11; i++){
-  num = getRandomInt(1, 11);
-  h=1;
-  while(h == 1){
-    k=0; j=0;
-    while(k < 11){
-      if(mond[k] === num){
-        j++;
+  for(i=0; i<11; i++){
+    num = getRandomInt(1, 11);
+    h=1;
+    while(h == 1){
+      k=0; j=0;
+      while(k < 11){
+        if(mond[k] === num){
+          j++;
+        }
+        k++;
       }
-      k++;
-    }
-    if(j === 0){
-      mond[i]=num;
-      h=2;
-    } else {
-      num = getRandomInt(1, 11);
+      if(j === 0){
+        mond[i]=num;
+        h=2;
+      } else {
+        num = getRandomInt(1, 11);
+      }
     }
   }
-}
-return mond;
+  return mond;
 }
 
 var doc = document,
@@ -39,8 +41,9 @@ var doc = document,
     pass=doc.getElementById("pass");
 
 var rand_num = Randrand(monk);
-var i = 0, j = 0, k=0, h=0, l=0;
+var i = 0, j = 0, k=0, h=0, l=0, g=0;
 var evil_amount = 0, hero_amount = 0;
+
 // Вывод первых двух карт противника и одного героя
 evil_amount = rand_num[i];
 card_evil[j].innerHTML = rand_num[i];
@@ -53,41 +56,46 @@ i++; j++;
 evil_amount = evil_amount + rand_num[i];
 card_evil[j].innerHTML = rand_num[i];
 amount_evil.innerHTML = evil_amount - rand_num[0];
+
 // Если кликнешь по еще, то дадут карту тебе и противнику
 more.onclick = function () {
-  i++; k++;
-  hero_amount = hero_amount + rand_num[i];
-  card_hero[k].innerHTML = rand_num[i];
-  amount_hero.innerHTML = hero_amount;
+  if(hero_amount < 22 && g === 0){
+    i++; k++;
+    hero_amount = hero_amount + rand_num[i];
+    card_hero[k].innerHTML = rand_num[i];
+    amount_hero.innerHTML = hero_amount;
+  }
   if((evil_amount + rand_num[i+1]) < 22){
-  j++; i++;
-  evil_amount = evil_amount + rand_num[i];
-  card_evil[j].innerHTML = rand_num[i];
-  amount_evil.innerHTML = evil_amount - rand_num[0];
+    j++; i++;
+    evil_amount = evil_amount + rand_num[i];
+    card_evil[j].innerHTML = rand_num[i];
+    amount_evil.innerHTML = evil_amount - rand_num[0];
   }else{
     h=1;
   }
 }
+
 // Если кликнешь по пропустить, то дадут карту противнику и проверят на наличие выйгрыша
 pass.onclick = function () {
   l=1;
   if((evil_amount + rand_num[i+1]) < 22){
-  i++; j++;
-  evil_amount = evil_amount + rand_num[i];
-  card_evil[j].innerHTML = rand_num[i];
-  amount_evil.innerHTML = evil_amount - rand_num[0];
+    i++; j++;
+    evil_amount = evil_amount + rand_num[i];
+    card_evil[j].innerHTML = rand_num[i];
+    amount_evil.innerHTML = evil_amount - rand_num[0];
   }else{
     h=1;
   }
   if(l == 1 && h == 1){
-  if((evil_amount < hero_amount)  && (hero_amount < 22)){
-    win_evil.innerHTML = "Поражение";
-    win_hero.innerHTML = "Победа";
-  }else{
-    win_hero.innerHTML = "Поражение";
-    win_evil.innerHTML = "Победа";
-  }
+    if((evil_amount < hero_amount)  && (hero_amount < 22)){
+      win_evil.innerHTML = "Поражение";
+      win_hero.innerHTML = "Победа";
+    }else{
+      win_hero.innerHTML = "Поражение";
+      win_evil.innerHTML = "Победа";
+    }
     card_evil[0].style.color = "black";
-  amount_evil.innerHTML = evil_amount;
-}
+    amount_evil.innerHTML = evil_amount;
+    g=1;
+  }
 }
